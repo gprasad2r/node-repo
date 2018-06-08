e8836eee898b8b43ff0bcec3ae457490b7b04234
prasad <gprasad2r@gmail.com>
2018-06-08 23:56:01 +0530

    setup home,about,projects pages

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const ejs =require('ejs');
 const app = express();
 
 const port = process.env.PORT || 3000;
+app.set('view engine','ejs');
+
+app.use(express.static(__dirname + "./public"));
 
 app.get('/', (req,res) => {
-	res.send("<h1>home</h1>");
+	res.render('home',{pageTitle:"welcome to my web site"});
+});
+
+app.get('/about', (req,res) => {
+	res.render('about',{pageTitle:"About page"});
+});
+
+app.get('/projects', (req,res) => {
+	res.render('projects',{pageTitle:"Projects page"});
 });
 
 app.listen(port, () => {
