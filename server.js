cdffca5993cfec84d49208a8acf7c2bcb6ff949c
prasad <gprasad2r@gmail.com>
2018-06-08 23:05:45 +0530

    added server config in server.js

--- server.js
+++ server.js
@@ -1,2 +1,12 @@
 const express = require('express');
 const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req,res) => {
+	res.send("home");
+});
+
+app.listen(port, () => {
+	console.log(`server has started on port ${port}`);
+})
